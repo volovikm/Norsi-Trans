d6b20a8f9a29db34f6c2a58ef9ad3b02371b80c3
Michael <volovik2000@inbox.ru>
2024-10-16 19:13:38 +0300

    Указаны связи таблиц в моделях

--- app/models/Assembly.php
+++ app/models/Assembly.php
@@ -6,5 +6,8 @@ use yii\db\ActiveRecord;
 
 class Assembly extends ActiveRecord
 {
-
+    public static function tableName()
+    {
+       return 'assembly';
+    }
 }
--- app/models/Assembly_item.php
+++ app/models/Assembly_item.php
@@ -6,5 +6,18 @@ use yii\db\ActiveRecord;
 
 class Assembly_item extends ActiveRecord
 {
+    public static function tableName()
+    {
+       return 'assembly_item';
+    }
 
+    //Связь с таблицей assembly
+    public function getAssembly() {
+        return $this->hasOne(Assembly::className(), ['id' => 'assembly_id']);
+    }
+
+    //Связь с таблицей item
+    public function getItem() {
+        return $this->hasOne(Item::className(), ['id' => 'item_id']);
+    }
 }
